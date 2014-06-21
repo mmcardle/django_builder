6769d0f81a171f6470546f6682939229d9959d23
mmcardle <mark.mcardle@sohonet.com>
2014-06-21 21:31:28 +0100

    Production Settings

--- django_builder/wsgi.py
+++ django_builder/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_builder.settings")
-os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Prod')
 
 from configurations.wsgi import get_wsgi_application
 
