7a835912f6382aade01022a8424cdedf1068d744
mmcardle <mark.mcardle@sohonet.com>
2015-06-10 07:58:29 +0100

    Adding build scripts

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh 
+cp ../django_builder/angular/app/index.html ./
+cp ../django_builder/angular/app/build/django_builder.min.* ./build/
+cp ../django_builder/angular/app/img/favicon.ico ./img/
+cp ../django_builder/angular/app/build/django_builder.min.* build/
+cp ../django_builder/angular/app/partials/*.html partials/
+cp ../django_builder/angular/app/partials/templates/detail.html partials/templates/
