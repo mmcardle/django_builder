5f17de09c5248830eeb7bf74599e0062427bf67b
mmcardle <mark.mcardle@sohonet.com>
2016-12-02 19:38:56 +0000

    Update build

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ cp ../django_builder/angular/app/build/django_builder.min.* ./build/
 cp ../django_builder/angular/app/img/favicon.ico ./img/
 cp ../django_builder/angular/app/partials/*.html partials/
 cp ../django_builder/angular/app/partials/templates/detail.html partials/templates/
+cp ../django_builder/angular/app/partials/py/* partials/py/
