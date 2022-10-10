a89da1c130ce5b44b19e7d93bd973c985c372234
Sean M. Collins <sean@coreitpro.com>
2022-10-10 11:49:35 -0400

    Update link to static-files howto
    2.1 is a very old version of django

--- src/django/python/settings.py
+++ src/django/python/settings.py
@@ -110,6 +110,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
