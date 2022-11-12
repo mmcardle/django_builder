0af52654c00fe84cee373edf5fbbd03cff79e64b
mmcardle <m.mc4rdle@gmail.com>
2022-11-12 23:01:01 +0000

    Update Tests

--- script/run.sh
+++ script/run.sh
@@ -12,4 +12,7 @@ source .venv/bin/activate
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver
+python manage.py runserver &
+ID=$!
+curl http://127.0.0.1:8080
+kill ${ID}
