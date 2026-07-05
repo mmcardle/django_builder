e0a2b916051180e785946995c1abeeeeb25781d5
Mark McArdle <m.mc4rdle@gmail.com>
2026-07-05 20:45:45 +0100

    feat(db5): vite/vitest config, tailwind v4, design tokens (dark+light)

--- packages/djangobuilder5/src/main.tsx
+++ packages/djangobuilder5/src/main.tsx
@@ -1,8 +1,11 @@
+import "@fontsource-variable/inter";
+import "@fontsource-variable/jetbrains-mono";
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>djangobuilder5</h1>
+    <h1 className="p-8 text-accent">djangobuilder5</h1>
   </StrictMode>,
 );
